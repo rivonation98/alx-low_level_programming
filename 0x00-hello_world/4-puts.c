fc8efcab1220173480773bcd70132095268cc6da
Nyiko <nyikosupermega@gmail.com>
2023-03-20 08:27:11 -0700

    task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 /**
  * main - a printing program that uses puts instead of printf to print a line
  *
- * return: Always 0 (success)
- */
-int main (void)
+ * return: 0 (success)
+ *  */
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\"\n");
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
 
