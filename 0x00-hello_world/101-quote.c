fd462faa849e1e32c7365230aa183ff5ddf4b808
Nyiko <nyikosupermega@gmail.com>
2023-03-20 11:10:29 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * Description: main - program that prints exactly and that piece of art is useful
- * *Return: 0 if success
+ * Description: main - program that prints exactly a message.
+ * *Return: 1 if success
  */
 int main(void)
 {
-write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
