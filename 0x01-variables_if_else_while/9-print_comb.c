4f1dda74fe7bf7f2c1a7250a4ba788f3cba756d7
Nyiko <nyikosupermega@gmail.com>
2023-03-20 14:39:50 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *  main - prints all possible combinations of single-digit numbers.
+ *  Return: 0 if success
+ */
+int main(void)
+
+{
+int n;
+for (n = 48; n < 58; n++)
+{
+putchar(n);
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
