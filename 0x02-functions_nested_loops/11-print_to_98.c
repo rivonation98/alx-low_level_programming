f9354cd7186380395d5fdedc5e1ea0aedb051c71
Nyiko <nyikosupermega@gmail.com>
2023-03-22 10:16:00 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,10 +12,10 @@ if (n <= 98)
 for (i = n; i <= 98; i++)
 {
 if (i != 98)
-printf(" % d, ", i);
+printf("%d,", i);
 else
 if (i == 98)
-printf(" % d\n", i);
+printf("%d\n", i);
 }
 }
 else
@@ -24,10 +24,10 @@ if (n >= 98)
 for (j = n; j >= 98; j--)
 {
 if (j != 98)
-printf(" % d, ", j);
+printf("%d,", j);
 else
 if (j == 98)
-printf(" % d\n", j);
+printf("%d\n", j);
 }
 }
 }
